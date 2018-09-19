cec21f18222ca0d461ce26a481aff7665d2eaeff
Shennie Yao <sheya@microsoft.com>
2018-09-19 15:20:08 -0700

    added updated package.json to pull in newer version of chrome core, also taking out undefined check and putting that into chrome core

--- src/edgeDebugAdapter.ts
+++ src/edgeDebugAdapter.ts
@@ -231,7 +231,8 @@ export class EdgeDebugAdapter extends CoreDebugAdapter {
             });
 
             let protocolVersion : TargetVersions = await this._chromeConnection.version;
-            this._edgeProtocolVersion = (protocolVersion === undefined) ? undefined : protocolVersion.protocol;
+            // this._chromeConnection.version is never undefined (will always be at least 0.0)
+            this._edgeProtocolVersion = protocolVersion.protocol;
 
             // Send the versions information as it's own event so we can easily backfill other events in the user session if needed
             userAgentForTelemetryPromise.then(versionInformation => telemetry.telemetry.reportEvent('target-version', versionInformation));
